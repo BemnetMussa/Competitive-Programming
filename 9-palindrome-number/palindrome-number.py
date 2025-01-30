1934001bbe24e9debd1a18c8fc62b405eb5f0a7b
Bemnet Mussa <117770007+BemnetMussa@users.noreply.github.com>
2025-01-30 16:28:53 +0300

    Time: 1 ms (93.19%) | Memory: 17.7 MB (67.00%) - LeetSync

--- 9-palindrome-number/palindrome-number.py
+++ 9-palindrome-number/palindrome-number.py
@@ -1,32 +1,6 @@
-# class Solution:
-#     def isPalindrome(self, x: int) -> bool:
-        
-#         y = x 
-#         r = 0
-#         while y > 0:
-#             rm = y % 10 
-#             r = (r * 10 + rm)
-#             y = y//10
-
-
-#         return True if y == x else False
-
-
 class Solution:
     def isPalindrome(self, x: int) -> bool:
-        if x < 0:
-            return False
+        
+        y = str(x)
+        return True if x >= 0 and x == int(y[::-1]) else False
 
-        # Copy the input number to another variable
-        original = x  # Save the original number
-        reversed_number = 0  # Initialize the reversed number
-
-        # Reverse the digits of the number
-        while x > 0:
-            digit = x % 10  # Get the last digit
-            reversed_number = reversed_number * 10 + digit  # Add the digit to the reversed number
-            print(reversed_number)
-            x = x // 10  # Remove the last digit from x
-
-        # Compare the reversed number with the original number
-        return reversed_number == original
