1f35e14ac077789d560d146a8ce78fb2574c158b
Bemnet Mussa <117770007+BemnetMussa@users.noreply.github.com>
2025-02-05 19:18:56 +0300

    Time: 309 ms (20.37%) | Memory: 57.9 MB (31.10%) - LeetSync

--- 380-insert-delete-getrandom-o1/insert-delete-getrandom-o1.py
+++ 380-insert-delete-getrandom-o1/insert-delete-getrandom-o1.py
@@ -1,21 +1,52 @@
 class RandomizedSet:
+
     def __init__(self):
-        self.randset = []
-    def insert(self, val: int) -> bool:
-        if val not in self.randset:
-            self.randset.append(val)
-            return True
-        return False
-    def remove(self, val: int) -> bool:
-        if val in self.randset:
-            self.randset.remove(val)
-            return True
-        return False
-    def getRandom(self) -> int:
-        l = randint(0, len(self.randset))
-        l-=1
-        return random.choice(self.randset)
+        self.dict_index = defaultdict(int)
+        self.arr = []
+                                                 
         
+
+    def insert(self, val: int) -> bool:
+        if val in self.arr:
+            return False
+    
+        self.arr.append(val)
+        self.dict_index[val] = len(self.arr) - 1
+
+        return True
+        
+
+    def remove(self, val: int) -> bool:
+        if val not in self.arr:
+            return False
+
+        # getting the index the value
+        index_val = self.dict_index[val]
+        # swap val to the last arr
+        self.swap(index_val, -1)
+
+        # poping the last 
+        self.arr.pop()
+        del self.dict_index[val]
+        return True
+        
+    
+    def swap(self, val_index, last_index):
+        temp = self.arr[val_index]
+        self.arr[val_index] = self.arr[last_index]
+        self.arr[last_index] = temp
+
+        # update the index of the swaped val
+        self.dict_index[self.arr[val_index]] = val_index
+
+
+    def getRandom(self) -> int:
+        return random.choice(self.arr)
+
+
+
+
+
 
 
 # Your RandomizedSet object will be instantiated and called as such:
