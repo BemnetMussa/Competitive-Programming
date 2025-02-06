81dafd44f18b9d28c4448b5cbf509831049a3b4c
Bemnet Mussa <117770007+BemnetMussa@users.noreply.github.com>
2025-02-06 16:00:12 +0300

    Time: 4 ms (8.40%) | Memory: 17.6 MB (75.75%) - LeetSync

--- 2015-determine-whether-matrix-can-be-obtained-by-rotation/determine-whether-matrix-can-be-obtained-by-rotation.py
+++ 2015-determine-whether-matrix-can-be-obtained-by-rotation/determine-whether-matrix-can-be-obtained-by-rotation.py
@@ -3,19 +3,13 @@ class Solution:
         row = len(mat)
         col = len(mat[0])
 
-        if mat == target:
-            return True
 
-        if len(mat) == 0:
-            return False
 
-        rotated_mat = self.rotate90(mat, row, col)
-
-        for i in range(1, 4):
-            print(rotated_mat)
-            if rotated_mat == target:
+        for i in range(0, 4):
+            if mat == target:
                 return True
-            rotated_mat = self.rotate90(rotated_mat, row, col)
+
+            mat = self.rotate90(mat, row, col)
 
         return False
 
@@ -25,19 +19,13 @@ class Solution:
     def rotate90(self, mat, row, col):
 
         new_mat = [[0] * row for _ in range(col)]
-        # [[1, 2, 3, 4], [1, 2, 3, 4]]
 
         for c in range(col):
-
-            # curr  [1, 2, 3, 4]
             if len(mat) != 0:
 
                 mat_list = mat.pop()
-                print(mat_list)
-
                 for r in range(row):
                     new_mat[r][c] = mat_list[r]
-            print(new_mat)
-        print('done')
+      
 
         return new_mat
