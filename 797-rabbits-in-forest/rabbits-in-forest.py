614a26f7f3071ffc44b82a5a6b6e0dbd178dc7b5
Bemnet Mussa <117770007+BemnetMussa@users.noreply.github.com>
2025-02-09 23:13:11 +0300

    Time: 0 ms (100.00%) | Memory: 18 MB (26.70%) - LeetSync

--- 797-rabbits-in-forest/rabbits-in-forest.py
+++ 797-rabbits-in-forest/rabbits-in-forest.py
@@ -2,25 +2,24 @@ class Solution:
     def numRabbits(self, answers: List[int]) -> int:
         
 
-        rabbits = 0
+        rab = 0
+        uniq = dict()
 
-        length = 0
-        uniq = set()
-        answers.sort()
+
 
         for ans in answers:
-            if ans in uniq:
-                if length == 0:
-                    rabbits += (1 + ans)
-                    length = ans
-                else:
-                    length -= 1
+            if ans == 0:
+                rab += 1
 
+            elif ans in uniq:
+                uniq[ans] -= 1
+                if uniq[ans] == 0:
+                    del uniq[ans]
             else:
-                rabbits += (1 + ans)
-                length = ans
-                uniq.add(ans)
+                rab += (ans + 1)
+                uniq[ans] = ans
+
 
         
-        return rabbits
+        return rab
                 
