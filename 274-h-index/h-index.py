22257ecc850472bd978dcc639363560a516e05ea
Bemnet Mussa <117770007+BemnetMussa@users.noreply.github.com>
2025-02-11 17:23:37 +0300

    Time: 1 ms (35.11%) | Memory: 18.1 MB (37.17%) - LeetSync

--- 274-h-index/h-index.py
+++ 274-h-index/h-index.py
@@ -3,16 +3,35 @@ class Solution:
         
         citations.sort()
 
-        left = 0
-        right = len(citations) -1
+        freq = [0] * (len(citations)+1)
 
-        while (left <= right):
-            mid = (left + right) // 2
+        citation = 0
+     
+        for i in range(len(citations)):
 
-            if citations[mid] >= len(citations) - mid:
-                right = mid - 1
-
+            if citations[i] >= len(freq):
+                freq[-1] += 1
             else:
-                left = mid + 1
+                freq[citations[i]] += 1
+        print(freq)
+        for i in range(len(freq)-2, -1, -1):
+            freq[i] += freq[i+1]
+        print(freq)
 
-        return  len(citations) - left
+        for i in range(len(freq)-1, -1, -1):
+            if i <= freq[i]:
+                return i
+
+        # left = 0
+        # right = len(citations) -1
+
+        # while (left <= right):
+        #     mid = (left + right) // 2
+
+        #     if citations[mid] >= len(citations) - mid:
+        #         right = mid - 1
+
+        #     else:
+        #         left = mid + 1
+
+        # return  len(citations) - left
