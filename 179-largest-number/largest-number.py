00faa3cc84d35c458720041ae70c1a8e54ec0757
Bemnet Mussa <117770007+BemnetMussa@users.noreply.github.com>
2025-02-11 10:41:16 +0300

    Time: 0 ms (100.00%) | Memory: 17.7 MB (79.48%) - LeetSync

--- 179-largest-number/largest-number.py
+++ 179-largest-number/largest-number.py
@@ -3,26 +3,11 @@ from functools import cmp_to_key
 class Solution:
     def largestNumber(self, nums):
 
-        nums = list(map(str, nums))
-    
-        def compare(x, y):
-            # return -1 to put x before y
-            if x + y > y + x:
-                return -1  
-            # return 1 to put y before x
-            elif x + y < y + x:
-                return 1 
-            # no change it will be placed in order
-            else:
-                return 0 
+        arr = [str(x) for x in nums]
+        arr.sort(key=lambda x:x*10, reverse = True)
 
-        print(nums)
+        if arr[0] == "0":
+            return "0"
 
-        nums.sort(key=cmp_to_key(compare))
-        
-        result = ''.join(nums)
-
-        if result[0] == '0':
-            return '0'
-        
-        return result
+        res = "".join(arr)
+        return res
