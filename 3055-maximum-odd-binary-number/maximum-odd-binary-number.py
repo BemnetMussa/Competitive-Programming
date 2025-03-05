1648de57bcac674a7d5e4dec0e94675b257ae1ec
Bemnet Mussa <117770007+BemnetMussa@users.noreply.github.com>
2025-03-05 16:12:55 +0300

    Time: 3 ms (38.64%) | Memory: 17.8 MB (37.47%) - LeetSync

--- 3055-maximum-odd-binary-number/maximum-odd-binary-number.py
+++ 3055-maximum-odd-binary-number/maximum-odd-binary-number.py
@@ -1,24 +1,11 @@
 class Solution:
     def maximumOddBinaryNumber(self, s: str) -> str:
-        
-        # counting the characters of 1
-        # substracting the number of 1 from the length of s = num of zeros
-        # taking -1 from the ones count for the last one : it works becase it is at least the string have 1 character of 1
-        
+
         ones = s.count("1") 
         zeros = len(s) - ones
 
-        ans = []
-        for i in range(ones-1):
-            ans.append("1")
 
-        for i in range(zeros):
-            ans.append("0")
-
-        # the one that we have substracted such that it will be on the last 
-        ans.append("1")
-
-        return "".join(ans)
+        return "1" * (ones-1) + "0" * zeros + "1"
         
 
 
