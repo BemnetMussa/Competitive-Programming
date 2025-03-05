11c941d928c23d3a1f4cc80742a074d163e6304c
Bemnet Mussa <117770007+BemnetMussa@users.noreply.github.com>
2025-03-05 17:45:56 +0300

    Time: 0 ms (100.00%) | Memory: 17.8 MB (80.77%) - LeetSync

---
+++ 20-valid-parentheses/valid-parentheses.py
@@ -0,0 +1,20 @@
+class Solution:
+    def isValid(self, s: str) -> bool:
+        zmap = {
+            "(": ")",
+            "[": "]",
+            "{": "}"
+        }
+
+
+        stack =[]
+
+        for char in s:
+            if char in zmap.keys():
+                stack.append(char)
+            elif stack and zmap[stack[-1]] == char:
+                stack.pop()
+            else:
+                return False
+
+        return True if not(stack) else False
