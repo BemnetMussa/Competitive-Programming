e8a4058c1bebeaab09a16ddd31b300fb6228c49b
Bemnet Mussa <117770007+BemnetMussa@users.noreply.github.com>
2025-03-06 17:50:11 +0300

    Time: 0 ms (100.00%) | Memory: 18 MB (40.58%) - LeetSync

--- 496-next-greater-element-i/next-greater-element-i.py
+++ 496-next-greater-element-i/next-greater-element-i.py
@@ -9,7 +9,7 @@ class Solution:
             while stack and stack[-1] < nums2[i]:
                 stack.pop()
 
-            if stack and stack[-1] > nums2[i]:
+            if stack:
                 next_map[nums2[i]] = stack[-1]
             else:
                 next_map[nums2[i]] = -1
@@ -19,7 +19,7 @@ class Solution:
         ans = []
         for num in nums1:
             ans.append(next_map[num])
-        print(next_map, ans)
+   
         return ans
 
         
