68606db107911505ca64697b99921fab95b8d48e
Bemnet Mussa <117770007+BemnetMussa@users.noreply.github.com>
2025-03-18 10:21:48 +0300

    Time: 92 ms (55.49%) | Memory: 31 MB (91.16%) - LeetSync

--- 2478-longest-nice-subarray/longest-nice-subarray.py
+++ 2478-longest-nice-subarray/longest-nice-subarray.py
@@ -6,11 +6,14 @@ class Solution:
         l = 0
 
         for r in range(len(nums)):
+            # when it overlaps
             while curr & nums[r]:
+                # unmask the the removed num[l]
                 curr = curr ^ nums[l]
                 l += 1
-            
+            # not overlaps
             res = max(res, r - l + 1)
-            curr = curr | nums[r]
+            curr = curr ^ nums[r]
+    
 
         return res
