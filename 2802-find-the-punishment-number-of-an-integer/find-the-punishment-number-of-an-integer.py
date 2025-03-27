74670f592c71b473526e173057f9a9622b371399
Bemnet Mussa <117770007+BemnetMussa@users.noreply.github.com>
2025-03-27 17:29:06 +0300

    Time: 1194 ms (27.22%) | Memory: 18 MB (33.32%) - LeetSync

--- 2802-find-the-punishment-number-of-an-integer/find-the-punishment-number-of-an-integer.py
+++ 2802-find-the-punishment-number-of-an-integer/find-the-punishment-number-of-an-integer.py
@@ -1,12 +1,18 @@
 class Solution:
     def punishmentNumber(self, n: int) -> int:
-        arr = [1,9,10,36,45,55,82,91,99,100,235,297,369,370,379,414,
-               657,675,703,756,792,909,918,945,964,990,991,999,1000]
-        
-        total = 0
-        for num in arr:
-            if num <= n:
-                total += num * num
-            else:
-                break
-        return total
+        def partition(i, curr, target, string):
+            if i == len(string) and curr == target:
+                return True
+            
+            for j in range(i, len(string)):
+                if partition(j+1, curr + int(string[i:j+1]), target, string):
+                    return True
+
+            return False
+
+        res = 0
+        for i in range(1, n+1):
+            if partition(0, 0, i, str(i*i)):
+                res += i * i
+
+        return res
