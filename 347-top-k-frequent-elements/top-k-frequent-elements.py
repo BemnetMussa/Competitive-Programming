0fccd8a880146af23e44bf0c08899df58623ed68
Bemnet Mussa <117770007+BemnetMussa@users.noreply.github.com>
2025-04-03 02:41:16 +0300

    Time: 7 ms (51.11%) | Memory: 24.2 MB (19.72%) - LeetSync

--- 347-top-k-frequent-elements/top-k-frequent-elements.py
+++ 347-top-k-frequent-elements/top-k-frequent-elements.py
@@ -1,14 +1,21 @@
 class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
-        
-        count_s = Counter(nums)
-    
-        sorted_dict = dict(sorted(count_s.items(), key=lambda x: x[1], reverse=True))
+        # Bucket sort Algorithm 
+
+        # bucket size == len(nums) at max one element will have len(nums) repeating nums
+        bucket = [[] for _ in range(len(nums)+1)]
+
+        freq_counter = Counter(nums)
+        for n, f in freq_counter.items():
+            bucket[f].append(n)
 
         ans = []
+        
+        for i in range(len(bucket)-1, -1, -1):
+            if bucket[i]:
+                ans.extend(bucket[i])
+                if len(ans) == k:
+                    return ans
 
-        for num in sorted_dict.keys():
-            if len(ans) < k:
-                ans.append(num)
 
-        return ans
+        
