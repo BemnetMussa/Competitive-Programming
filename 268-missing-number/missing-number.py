24b56647357eaaf5b1ee4994615556b000f834e7
Bemnet Mussa <117770007+BemnetMussa@users.noreply.github.com>
2025-04-03 21:32:26 +0300

    Time: 0 ms (100.00%) | Memory: 18.7 MB (73.11%) - LeetSync

--- 268-missing-number/missing-number.py
+++ 268-missing-number/missing-number.py
@@ -1,10 +1,7 @@
 class Solution:
     def missingNumber(self, nums: List[int]) -> int:
-        nums = set(nums) # for fast look ups
-        index = 0
+        n = len(nums)
+        total_sum = n * (n + 1) // 2 # sum of the first n numbers
+        actual_sum = sum(nums) # actual sum
 
-        for i in range(len(nums)+1):
-            if not(i in nums):
-                index = i
-
-        return index
+        return total_sum - actual_sum # the missing one
