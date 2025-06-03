210a6b9cd6aa3c616e130eaa77d9cc0436c0c10c
Bemnet Mussa <117770007+BemnetMussa@users.noreply.github.com>
2025-06-04 01:26:21 +0300

    Time: 7 ms (97.29%) | Memory: 18.3 MB (35.50%) - LeetSync

--- 51-n-queens/n-queens.py
+++ 51-n-queens/n-queens.py
@@ -1,30 +1,35 @@
 class Solution:
     def solveNQueens(self, n: int) -> List[List[str]]:
-        def issafe(r,c):
-            n = len(board)
-            for i in range(n):
-                if board[i][c] == 'Q':
-                    return False
-                if r - i >= 0 and c - i >= 0 and board[r-i][c-i] == 'Q':
-                    return False
-                if r - i >= 0 and c + i < n and board[r-i][c+i] == 'Q':
-                    return False
-            return True
-                
-        def solve(r):
-            n = len(board)
-            if r == n:
-                print(board)
-                ans.append(["".join(i) for i in board])
-                return 
-            for c in range(0,n):
-                if issafe(r,c):
-                    board[r][c] = 'Q'
-                    solve(r+1)
-                    board[r][c] = '.'
+        cols = set()
+        postivDiag = set()
+        negativeDiag = set()
 
-                    
-        board = [['.']*n for i in range(n)]
-        ans =[]
-        solve(0) 
-        return ans
+        board = [["."] * n for _ in range(n)]
+
+        res= []
+        def backtrack(r):
+            #base case
+            if r == n:
+                copy = ["".join(row) for row in board]
+                res.append(copy)
+                return
+            
+            for c in range(n): # for each row
+                if c in cols or (r + c) in postivDiag or (r - c) in negativeDiag:
+                    continue
+                
+                cols.add(c)
+                postivDiag.add(r+c)
+                negativeDiag.add(r-c)
+                board[r][c] = "Q"
+
+                backtrack( r + 1 ) # going to the next row
+
+                cols.remove(c)
+                postivDiag.remove(r + c)
+                negativeDiag.remove(r-c)
+                board[r][c] = "."
+
+        backtrack(0)
+        return res
+    
