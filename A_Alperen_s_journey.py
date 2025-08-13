f3ca6dbe12d98f631c104bf33c0a35ae2e60d4d0
bemnetmussa <bemnetmuss@gmail.com>
2025-08-13 19:43:10 +0300

    update problemset

---
+++ A_Alperen_s_journey.py
@@ -0,0 +1,58 @@
+# -----------------------------
+# Language: Python 3
+# Author: Bemnet
+# -----------------------------
+
+import sys
+from math import ceil, floor, sqrt, gcd, inf, log2, isqrt, lcm
+import itertools
+import heapq as heap
+from collections import defaultdict, Counter, deque
+from bisect import bisect_left, bisect_right
+from random import randint
+
+input = sys.stdin.readline
+
+number = lambda: int(input().strip())
+numbers = lambda: list(map(int, input().strip().split()))
+so = lambda: sorted(map(int, input().strip().split()))
+word = lambda: input().strip()
+words = lambda: input().strip().split()
+yn = lambda cond: "YES" if cond else "NO"
+prefix_sum = lambda arr: list(itertools.accumulate(arr))
+rand = randint(1, 10**9)
+xor = lambda x: x ^ rand
+
+def test_cases(custom_cases=0):
+    return number() if not custom_cases else custom_cases
+
+def to_min_heap(arr):
+    heap.heapify(arr)
+    return arr
+
+def to_max_heap(arr):
+    heap.heapify(arr := [-x for x in arr])
+    return arr
+
+def solve():
+    number() 
+    s = word()
+
+    curr_pos = (0, 0)
+    directions = {"U": (1, 0), "R": (0, 1), "L": (0, -1), "D": (-1, 0)}
+
+    for char in s:
+        dx, dy = directions[char]
+        curr_pos = (curr_pos[0] + dx, curr_pos[1] + dy)
+        if curr_pos == (1, 1):
+            print("YES")
+            return
+
+    print("NO")
+
+    
+
+if __name__ == "__main__":
+    for _ in range(test_cases()):
+        solve()
+                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                   
