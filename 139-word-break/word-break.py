90430ac8b30ca08985f5e0c0d38bf8f9fe6c391b
Bemnet Mussa <117770007+BemnetMussa@users.noreply.github.com>
2025-09-29 16:24:32 +0300

    Time: 5 ms (31.34%) | Memory: 18.1 MB (25.27%) - LeetSync

--- 139-word-break/word-break.py
+++ 139-word-break/word-break.py
@@ -2,17 +2,22 @@ class Solution:
     def wordBreak(self, s: str, wordDict: List[str]) -> bool:
         N = len(s)
 
-        @lru_cache(None)
+        memo = {}
         def dp(idx):
             # base case
             if idx >= N:
                 return True
             
+            if idx in memo:
+                return memo[idx]
+
             for j in range(idx+1, len(s)+1):
                 if s[idx:j] in wordDict and dp(j):
-                    return True
+                    memo[idx] = True
+                    return memo[idx]
 
-            return False
+            memo[idx] = False
+            return memo[idx]
 
         return dp(0)
 
