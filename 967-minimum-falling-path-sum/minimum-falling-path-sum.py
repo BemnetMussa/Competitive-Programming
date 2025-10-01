b681e31f55e0828c34fcd0b5919e3f26a0529e7b
Bemnet Mussa <117770007+BemnetMussa@users.noreply.github.com>
2025-10-01 16:13:58 +0300

    Time: 18 ms (74.95%) | Memory: 18.7 MB (78.80%) - LeetSync

--- 967-minimum-falling-path-sum/minimum-falling-path-sum.py
+++ 967-minimum-falling-path-sum/minimum-falling-path-sum.py
@@ -1,39 +1,14 @@
-'''
-Given:
-- Given an n x n array of integers matrix,
-
-Requested:
-# - Return the minimum sum of any falling path through matrix.
-  - 
-
-'''
-
-
-
-
-
 class Solution:
     def minFallingPathSum(self, matrix: List[List[int]]) -> int:
         ROW, COL = len(matrix), len(matrix[0])
+        dp = [row[:] for row in matrix]  # copy matrix for DP
         
-        memo = {}  # for memoization
+        for i in range(1, ROW):
+            for j in range(COL):
+                dp[i][j] += min(
+                    dp[i-1][j], 
+                    dp[i-1][j-1] if j-1 >= 0 else float('inf'), 
+                    dp[i-1][j+1] if j+1 < COL else float('inf')
+                )
         
-        def dp(i, j):
-            if j < 0 or j >= COL:  # out of bounds
-                return float('inf')
-            if i == ROW - 1:  # last row
-                return matrix[i][j]
-            if (i, j) in memo:
-                return memo[(i, j)]
-            
-            memo[(i, j)] = matrix[i][j] + min(
-                dp(i+1, j-1),
-                dp(i+1, j),
-                dp(i+1, j+1)
-            )
-            return memo[(i, j)]
-        
-        return min(dp(0, j) for j in range(COL))  # start from any column in first row
-
-
-        
+        return min(dp[-1])
