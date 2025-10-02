b505a1f7e218ed2b9867f6b35839fc926437c44a
Bemnet Mussa <117770007+BemnetMussa@users.noreply.github.com>
2025-10-02 22:17:35 +0300

    Time: 200 ms (63.39%) | Memory: 17.9 MB (78.58%) - LeetSync

--- 518-coin-change-ii/coin-change-ii.py
+++ 518-coin-change-ii/coin-change-ii.py
@@ -10,21 +10,31 @@ class Solution:
     def change(self, amount: int, coins: List[int]) -> int:
         N_coins = len(coins)
         # Top-down approche
-        memo = {}
-        def numCombinations_thatMakesUpTheAmount(idx, total):
-            if total == amount:
-                return 1
+        # memo = {}
+        # def numCombinations_thatMakesUpTheAmount(idx, total):
+        #     if total == amount:
+        #         return 1
 
-            if total > amount or idx == N_coins:
-                return 0
+        #     if total > amount or idx == N_coins:
+        #         return 0
             
-            if (idx, total) in memo:
-                return memo[(idx,total)]
+        #     if (idx, total) in memo:
+        #         return memo[(idx,total)]
 
-            res = numCombinations_thatMakesUpTheAmount(idx, total + coins[idx]) + numCombinations_thatMakesUpTheAmount(idx + 1, total)
+        #     res = numCombinations_thatMakesUpTheAmount(idx, total + coins[idx]) + numCombinations_thatMakesUpTheAmount(idx + 1, total)
+        #     memo[(idx, total)] = res
+        #     return memo[(idx, total)]
 
-            memo[(idx, total)] = res
-            return memo[(idx, total)]
-
-        return numCombinations_thatMakesUpTheAmount(0, 0)
+        # return numCombinations_thatMakesUpTheAmount(0, 0)
         
+
+        # Bottom-up approche
+        dp = [0] * ( amount + 1 )
+        dp[0] = 1
+
+        for coin in coins:
+            for i in range(coin, amount + 1):
+                dp[i] += dp[i - coin]
+
+        return dp[amount]
+
