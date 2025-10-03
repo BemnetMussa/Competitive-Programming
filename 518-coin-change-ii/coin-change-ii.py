35fce1fad66b5e4f486ea23a6b4ab36317cb44c8
Bemnet Mussa <117770007+BemnetMussa@users.noreply.github.com>
2025-10-03 11:29:27 +0300

    Time: 187 ms (69.11%) | Memory: 18.2 MB (59.42%) - LeetSync

--- 518-coin-change-ii/coin-change-ii.py
+++ 518-coin-change-ii/coin-change-ii.py
@@ -30,11 +30,11 @@ class Solution:
 
         # Bottom-up approche
         dp = [0] * ( amount + 1 )
-        dp[0] = 1
-
+        dp[0] = 1 # basecase when amount == coins
+      
         for coin in coins:
             for i in range(coin, amount + 1):
                 dp[i] += dp[i - coin]
 
-        return dp[amount]
+        return dp[amount] #O(n) spece complexity time of O(amount * coins)
 
