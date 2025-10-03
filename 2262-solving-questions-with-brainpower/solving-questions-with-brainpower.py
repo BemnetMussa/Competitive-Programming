f74d846ef098992b26378697e3842c5c8a23b75c
Bemnet Mussa <117770007+BemnetMussa@users.noreply.github.com>
2025-10-03 12:36:58 +0300

    Time: 144 ms (32.87%) | Memory: 68.1 MB (26.88%) - LeetSync

--- 2262-solving-questions-with-brainpower/solving-questions-with-brainpower.py
+++ 2262-solving-questions-with-brainpower/solving-questions-with-brainpower.py
@@ -1,12 +1,46 @@
-class Solution(object):
-    def mostPoints(self, questions):
-        dp = [0] * len(questions)
-        for i in range(len(questions) - 1, -1, -1):
-            index = i + questions[i][1] + 1
-            if index < len(questions):
-                dp[i] = dp[index] + questions[i][0]
-            else:
-                dp[i] = questions[i][0]
-            if i < len(questions) - 1:
-                dp[i] = max(dp[i + 1], dp[i])
-        return dp[0]
+"""
+Question:
+- 0-indexed 2D integer array questions where questions[i] = [pointsi, brainpoweri].
+- quesions must be in-order starting from question 0
+- Solving question i will earn you pointsi points but you will be unable to solve each of the next brainpoweri questions.
+
+Constraints:
+-   1 <= questions.length <= 105
+    questions[i].length == 2
+    1 <= pointsi, brainpoweri <= 105
+
+Requested: 
+- Return the maximum points you can earn from the exam.
+
+Approche:
+- questions = [[3,2],[4,3],[4,4],[2,5]]     ans = 7
+                
+                
+"""
+
+
+
+class Solution:
+    def mostPoints(self, questions: List[List[int]]) -> int:
+        N = len(questions)
+
+        # Top-Down approch
+        memo = {}
+        def dp(idx):
+            if idx >= N:
+                return 0
+            
+            if idx in memo:
+                return memo[idx]
+
+          
+            # solve current question 
+            profit = questions[idx][0] + dp(idx + questions[idx][1] + 1)
+
+            # skip current question
+            skip = dp(idx + 1)
+
+            memo[idx] = max(profit, skip)
+            return memo[idx]
+
+        return dp(0)
