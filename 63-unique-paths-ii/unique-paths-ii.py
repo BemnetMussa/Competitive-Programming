9a9a9993ff2aad57d17493a645ec94f563508d29
Bemnet Mussa <117770007+BemnetMussa@users.noreply.github.com>
2025-10-04 15:47:44 +0300

    Time: 0 ms (100.00%) | Memory: 17.8 MB (91.48%) - LeetSync

--- 63-unique-paths-ii/unique-paths-ii.py
+++ 63-unique-paths-ii/unique-paths-ii.py
@@ -17,28 +17,53 @@ class Solution:
         
         ROWS, COLS = len(obstacleGrid), len(obstacleGrid[0])
 
-        memo = {}
-        def dp(i, j):
-            if i >= ROWS or j >= COLS: return 0
-            if obstacleGrid[i][j] == 1:
-                return 0
+        # top down approche
+        # memo = {}
+        # def dp(i, j):
+        #     if i >= ROWS or j >= COLS: return 0
+        #     if obstacleGrid[i][j] == 1:
+        #         return 0
+        #     if (i, j) == (ROWS - 1, COLS - 1):
+        #         return 1
 
-            if (i, j) == (ROWS - 1, COLS - 1):
-                return 1
+        #     # memoization
+        #     if (i, j) in memo:
+        #         return memo[(i, j)]
 
-            # memoization
-            if (i, j) in memo:
-                return memo[(i, j)]
+        #     # paths
+        #     down = dp(i+1, j)
+        #     right = dp(i, j+1)
 
-            # paths
-            down = dp(i+1, j)
-            right = dp(i, j+1)
+        #     memo[(i, j)] = down + right
+        #     return memo[(i, j)]
 
-            memo[(i, j)] = down + right
+        # return dp(0, 0) # TIME, SPACE = O(N * M), O(N * M)
 
-            return memo[(i, j)]
 
-        return dp(0, 0)
+        # Bottom-up approch 
+        dp = [[0] * (COLS+1) for _ in range(ROWS+1)]
+
+        # Initilaize 
+        dp[ROWS-1][COLS-1] = 1
+
+        if obstacleGrid[ROWS-1][COLS-1] == 1:
+            return 0
+        
+
+        for i in range(ROWS-1, -1, -1):
+            for j in range(COLS-1, -1, -1):
+                if obstacleGrid[i][j] != 1:
+                    if i + 1 < ROWS:
+                        dp[i][j] += dp[i+1][j]
+                    if j + 1 < COLS:
+                        dp[i][j] += dp[i][j+1]
+
+        return dp[0][0]
+
+                
+
+
+
 
 
             
