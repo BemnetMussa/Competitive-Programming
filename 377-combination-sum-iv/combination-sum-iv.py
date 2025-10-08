6c75e5898a02aab9b7ec46b527e0f4eac23c54c9
Bemnet Mussa <117770007+BemnetMussa@users.noreply.github.com>
2025-10-08 16:21:03 +0300

    Time: 42 ms (63.92%) | Memory: 17.6 MB (97.40%) - LeetSync

--- 377-combination-sum-iv/combination-sum-iv.py
+++ 377-combination-sum-iv/combination-sum-iv.py
@@ -7,20 +7,31 @@ class Solution:
         # approche: since every combination will be considered then recursive approche will be the way to go DP
         
         n = len(nums)
-        # Top-Down approch
-        
-        @lru_cache(None)
-        def dp(curr_sum):
-            if curr_sum == target:
-                return 1
-            if curr_sum > target:
-                return 0
+        # # Top-Down approch
 
-            comb = 0
-            for i in range(n):
-                comb += dp(curr_sum + nums[i])
+        # @lru_cache(None)
+        # def dp(curr_sum):
+        #     if curr_sum == target:
+        #         return 1
+        #     if curr_sum > target:
+        #         return 0
 
-            return comb
+        #     comb = 0
+        #     for i in range(n):
+        #         comb += dp(curr_sum + nums[i])
 
-        return dp(0)
+        #     return comb
+
+        # return dp(0) # O(2^m) time and space complexity
+
+        # Bottom-Down approch
+        dp = [0] * (target + 1)
+        dp[0] = 1  # base case: one way to make sum 0
+
+        for total in range(1, target + 1):
+            for num in nums:
+                if total - num >= 0:
+                    dp[total] += dp[total - num]
+
+        return dp[target] # O(m*n)
 
