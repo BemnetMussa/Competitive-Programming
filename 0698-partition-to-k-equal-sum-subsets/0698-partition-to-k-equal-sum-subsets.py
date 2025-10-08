711b7b75b1bb18d401ca30b6f5a5608a6da7445c
Bemnet Mussa <117770007+BemnetMussa@users.noreply.github.com>
2025-10-08 12:41:03 +0300

    Time: 153 ms (40.08%), Space: 34.2 MB (16.1%) - LeetHub

--- 0698-partition-to-k-equal-sum-subsets/0698-partition-to-k-equal-sum-subsets.py
+++ 0698-partition-to-k-equal-sum-subsets/0698-partition-to-k-equal-sum-subsets.py
@@ -24,69 +24,44 @@ psudocode soltuion:
 
 '''
 
-
-# class Solution:
-#     def canPartitionKSubsets(self, nums: List[int], k: int) -> bool:
-#         N = len(nums)
-
-#         target = sum(nums) // k
-   
-#         # track the used elements
-#         used = [False] * N
-# \
-#         def divideArrayToKsubsets(idx, k, curr_sum):
-#             if k == 0:
-#                 return True
-#             if curr_sum == target:
-#                 return divideArrayToKsubsets(0, k-1, 0)
-
-#             for i in range(idx, N):
-#                 if not used[i] and curr_sum + nums[i] <= target:
-#                     used[i] = True
-#                     if divideArrayToKsubsets(i + 1, k, curr_sum + nums[i]):
-#                         return True
-#                     used[i] = False
-
-#             return False
-
-#         return divideArrayToKsubsets(0, k, 0)
-
-
-
 class Solution:
     def canPartitionKSubsets(self, nums: List[int], k: int) -> bool:
         total = sum(nums)
         if total % k != 0:
             return False
+
         target = total // k
-        nums.sort(reverse=True)  # optimization
-        used = [False] * len(nums)
+        
+        nums.sort(reverse=True)  # sake for optimizaiton
+       
         memo = {}
 
-        def backtrack(k, subsetSum, start):
-            state = tuple(used)
-            if state in memo:
-                return memo[state]
+        def backtrack(k, subsetSum, start, mask):
+            
+            if mask in memo:
+                return memo[mask]
             if k == 1:
                 return True
+
             if subsetSum == target:
-                res = backtrack(k - 1, 0, 0)
-                memo[state] = res
+                res = backtrack(k - 1, 0, 0, mask)
+                memo[mask] = res
                 return res
 
             for i in range(start, len(nums)):
-                if used[i] or subsetSum + nums[i] > target:
+                if mask & (1 << i) or subsetSum + nums[i] > target:
                     continue
-                used[i] = True
-                if backtrack(k, subsetSum + nums[i], i + 1):
-                    memo[state] = True
-                    return True
-                used[i] = False
 
-            memo[state] = False
+                new_mask = mask | (1 << i)
+                if backtrack(k, subsetSum + nums[i], i + 1, new_mask):
+                    memo[mask] = True
+                    return True
+                
+
+            memo[mask] = False
             return False
 
-        return backtrack(k, 0, 0)
+        return backtrack(k, 0, 0, 0)
 
 
 
