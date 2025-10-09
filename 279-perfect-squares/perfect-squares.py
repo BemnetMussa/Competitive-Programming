55ca6ce1f9bf30080e554e3217f2ed0bff873429
Bemnet Mussa <117770007+BemnetMussa@users.noreply.github.com>
2025-10-09 10:52:12 +0300

    Time: 2295 ms (64.53%) | Memory: 17.9 MB (73.13%) - LeetSync

--- 279-perfect-squares/perfect-squares.py
+++ 279-perfect-squares/perfect-squares.py
@@ -4,18 +4,32 @@ class Solution:
         # requested to return least number of perfect square numbers that sum up to n
 
         # approche: dp approche since to consider every possiblity 
+        # Top-down approch 
         squares = [i*i for i in range(1, isqrt(n)+1)]
 
-        @lru_cache(None)
-        def dp(remaining):
-            if remaining == 0:
-                return 0
+        # @lru_cache(None)
+        # def dp(remaining):
+        #     if remaining == 0:
+        #         return 0
 
-            min_count = float('inf')
+        #     min_count = float('inf')
+        #     for sq in squares:
+        #         if sq <= remaining:
+        #             min_count = min(min_count, 1 + dp(remaining - sq))
+
+        #     return min_count
+
+        # return dp(n)
+
+        # Bottom up approch:
+        dp = [float("inf")] * (n + 1)
+        dp[0] = 0
+
+        for num in range(1, n+1):
             for sq in squares:
-                if sq <= remaining:
-                    min_count = min(min_count, 1 + dp(remaining - sq))
+                if sq <= num:
+                    dp[num] = min(dp[num], 1 + dp[num - sq])
 
-            return min_count
+        return dp[n]
+        
 
-        return dp(n)
