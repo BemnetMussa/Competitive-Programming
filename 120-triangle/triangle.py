333459e39612f7ebc102db9ddf720a598f96a195
Bemnet Mussa <117770007+BemnetMussa@users.noreply.github.com>
2025-10-09 11:39:53 +0300

    Time: 3 ms (87.26%) | Memory: 19.2 MB (39.69%) - LeetSync

--- 120-triangle/triangle.py
+++ 120-triangle/triangle.py
@@ -7,16 +7,24 @@ class Solution:
         #constraints t <= triangle.length <= 200
         n = len(triangle)
 
-        @lru_cache(None)
-        def dfs(row, col):
-            # base case
-            if row == (n-1):
-                return triangle[row][col]
+        # @lru_cache(None)
+        # def dfs(row, col):
+        #     # base case
+        #     if row == (n-1):
+        #         return triangle[row][col]
 
-            # decision
-            down = dfs(row+1, col)
-            right = dfs(row+1, col+1)
+        #     # decision
+        #     down = dfs(row+1, col)
+        #     right = dfs(row+1, col+1)
 
-            return triangle[row][col] + min(down, right)
+        #     return triangle[row][col] + min(down, right)
 
-        return dfs(0, 0)
+        # return dfs(0, 0)
+
+        # bottom up approch        
+        for row in range(len(triangle) - 2, -1, -1):
+            for col in range(len(triangle[row])):
+
+                triangle[row][col] += min(triangle[row + 1][col], triangle[row + 1][col + 1])
+
+        return triangle[0][0]
