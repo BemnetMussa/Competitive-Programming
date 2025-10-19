767eff57c8a18a869e4d305205489a16c97c2220
Bemnet Mussa <117770007+BemnetMussa@users.noreply.github.com>
2025-10-19 15:00:40 +0300

    Time: 162 ms (33.14%) | Memory: 57.3 MB (92.75%) - LeetSync

--- 380-insert-delete-getrandom-o1/insert-delete-getrandom-o1.py
+++ 380-insert-delete-getrandom-o1/insert-delete-getrandom-o1.py
@@ -1,56 +1,35 @@
 class RandomizedSet:
-
     def __init__(self):
-        self.dict_index = defaultdict(int)
-        self.arr = []
-                                                 
-        
+        self.store = []
+        self.map_index = {}
 
     def insert(self, val: int) -> bool:
-        if val in self.dict_index:
-            return False
-    
-        self.arr.append(val)
-        self.dict_index[val] = len(self.arr) - 1
-
-        return True
-        
-
-    def remove(self, val: int) -> bool:
-        if val not in self.dict_index:
+        if val in self.map_index:
             return False
 
-        # getting the index the value
-        index_val = self.dict_index[val]
-        # swap val to the last arr
-        self.swap(index_val, -1)
+        self.store.append(val)
+        self.map_index[val] = len(self.store) - 1
 
-        # poping the last 
-        self.arr.pop()
-        del self.dict_index[val]
         return True
+
+
+    def remove(self, val: int) ->  bool:
+        if val not in self.map_index:
+            return False
         
-    
-    def swap(self, val_index, last_index):
-        temp = self.arr[val_index]
-        self.arr[val_index] = self.arr[last_index]
-        self.arr[last_index] = temp
+        last_indx = len(self.store) - 1
+        val_index = self.map_index[val]
 
-        # update the index of the swaped val
-        self.dict_index[self.arr[val_index]] = val_index
+        self.store[last_indx], self.store[val_index] = self.store[val_index], self.store[last_indx]
+        
+        self.map_index[self.store[val_index]]  = val_index
 
+        del self.map_index[val]
+        self.store.pop()
+
+        return True
 
     def getRandom(self) -> int:
-        random_index = random.randint(0, len(self.arr) - 1)
-        return self.arr[random_index]
+        index = random.randint(0, len(self.store) - 1)
+        return self.store[index]
 
-
-
-
-
-
-# Your RandomizedSet object will be instantiated and called as such:
-# obj = RandomizedSet()
-# param_1 = obj.insert(val)
-# param_2 = obj.remove(val)
-# param_3 = obj.getRandom()
