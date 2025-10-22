0f4d056b7424daa92b20cc6c16ce1cbed83bce5a
Bemnet Mussa <117770007+BemnetMussa@users.noreply.github.com>
2025-10-22 13:50:45 +0300

    Time: 7 ms (19.18%) | Memory: 18.6 MB (17.60%) - LeetSync

--- 73-set-matrix-zeroes/set-matrix-zeroes.py
+++ 73-set-matrix-zeroes/set-matrix-zeroes.py
@@ -1,31 +1,64 @@
+'''
+Given: m by n integer matrix. if an element is 0 set it's entire row and column to 0
+in place.
+
+approch: 
+[   [1,0,1],
+    [0,0,0],
+    [1,0,1]
+]
+
+# idetnified the indexs of the 0 and store it on an array[(i, j)]
+# going through it and change it's row and column to zero 
+
+O(n) and time of O(n*m)
+ optimize: 
+ assing the index that we got assign unique character that is *$
+ going throug the dollar sign and update it.
+
+O(1) and time of O(n*m)
+
+[   [*,1,2,*],
+    [3,4,5,2],
+    [1,3,1,5]
+]
+
+[   [0,0,0,0],
+    [0,4,5,2],
+    [0,3,1,5]
+]
+'''
+
 class Solution:
     def setZeroes(self, matrix: List[List[int]]) -> None:
         """
         Do not return anything, modify matrix in-place instead.
-        """
-        
-        # to hold the row and col of the zeros
-        set_row = set()
-        set_col = set()
+        """     
+        ROWS, COLS = len(matrix), len(matrix[0])
 
+        def helper(i, j): # helps to convert the rows and cols of the from the index
+            for x in range(ROWS):
+                if matrix[x][j] != "*":
+                    matrix[x][j] = 0
 
-        row = len(matrix)
-        col = len(matrix[0])
+            for y in range(COLS):
+                if matrix[i][y] != "*":
+                    matrix[i][y] = 0
+            matrix[i][j] = 0
 
-        for i in range(row):
-            for j in range(col):
-
+   
+        # mark the zero's part
+        for i in range(ROWS):
+            for j in range(COLS):
                 if matrix[i][j] == 0:
-                    set_row.add(i)
-                    set_col.add(j)
+                    matrix[i][j] = "*"
 
         
-        for i in range(row):
-            for j in range(col):
-                
-                if i in set_row or j in set_col:
-                    matrix[i][j] = 0
+        # change rows and cols of the stars *
+        for i in range(ROWS):
+            for j in range(COLS):
+                if matrix[i][j] == "*":
+                    helper(i, j)
 
         
 
-        print(matrix)
