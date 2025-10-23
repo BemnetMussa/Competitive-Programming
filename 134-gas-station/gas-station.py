2e76a63d004f8f7aca31664e805d30c2ab6110f0
Bemnet Mussa <117770007+BemnetMussa@users.noreply.github.com>
2025-10-23 12:50:09 +0300

    Time: 12 ms (95.11%) | Memory: 23.4 MB (43.85%) - LeetSync

--- 134-gas-station/gas-station.py
+++ 134-gas-station/gas-station.py
@@ -9,26 +9,21 @@ Solution:
          3,4,5,1,2,3,4,5,1,2
              |         |      --> O(n^2) 
 
-  (gas, cost)
-  [(1, 3), (2, 4), (3, 5), (4, 1), (5, 2)]
-    gas-cost
-  [(-2), [-2], [-2], [3], [3]]
-   
+
 '''
 class Solution:
     def canCompleteCircuit(self, gas: List[int], cost: List[int]) -> int:
-        
+        if sum(gas) < sum(cost):
+            return - 1
+
         total_tank = 0
-        curr_tank = 0
         start = 0
 
         for i in range(len(gas)):
             total_tank += gas[i] - cost[i]
-            curr_tank += gas[i] - cost[i]
-
-            if curr_tank < 0:
+   
+            if total_tank < 0:
                 start = i + 1
-                curr_tank = 0 
+                total_tank = 0
 
-        
-        return start if total_tank >= 0 else -1
+        return start 
