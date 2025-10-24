d33f162e81cc5b64bf97c69efc4e375e9685e78d
Bemnet Mussa <117770007+BemnetMussa@users.noreply.github.com>
2025-10-24 17:18:29 +0300

    Time: 0 ms (100.00%) | Memory: 17.8 MB (43.45%) - LeetSync

--- 1095-two-city-scheduling/two-city-scheduling.py
+++ 1095-two-city-scheduling/two-city-scheduling.py
@@ -1,16 +1,26 @@
+'''
+costs = [[10,20],[30,200],[400,50],[30,20]]
+Requested: to return the minijmum cost to fly eveyr person to a city such that exactly n people arraive in each city.
+
+[[10,20],[30,200],[400,50],[30,20]]
+  | 
+  
+'''
+
+
+
 class Solution:
     def twoCitySchedCost(self, costs: List[List[int]]) -> int:
-        
-        costs.sort(key=lambda x: (x[1] - x[0]))
 
-        n = len(costs)//2
-        print(costs)
-        ans = 0
+        # sort them based on the difference
+        costs.sort(key=lambda x: x[1] - x[0]) # respect of b - a
+        n = len(costs) // 2
+        total = 0
 
-        for a , b in costs[:n]:
-            ans += b
+        for i in range(n):
+            total += costs[i][1] 
 
-        for a , b in costs[n:]:
-            ans += a
-        
-        return ans
+        for i in range(n, n*2):
+            total += costs[i][0]
+
+        return total
