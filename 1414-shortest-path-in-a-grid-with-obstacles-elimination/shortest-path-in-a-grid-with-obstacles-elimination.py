bcbc7880e8acafd36cad7f1c771c02bed486eca1
Bemnet Mussa <117770007+BemnetMussa@users.noreply.github.com>
2025-10-25 15:39:12 +0300

    Time: 384 ms (40.15%) | Memory: 26.3 MB (10.33%) - LeetSync

--- 1414-shortest-path-in-a-grid-with-obstacles-elimination/shortest-path-in-a-grid-with-obstacles-elimination.py
+++ 1414-shortest-path-in-a-grid-with-obstacles-elimination/shortest-path-in-a-grid-with-obstacles-elimination.py
@@ -1,31 +1,51 @@
-from typing import List
-from collections import deque
+'''
+Given: m x n integer matrix grid. each cell is either 0 ( empthy ) or 1 ( obstacle ). up, down, rig, left, 
+from top (0, 0) reach (row-1, col-1) 
+
+Requested: Return the minimum number of steps that are needed. can remove k obstacles
+constratins: 1 <= m, n <= 40
+            1 <= k <= m * n
+approche: 
+-> [0,0,0],
+   [1,1,0],
+   [0,0,0],
+   [0,1,1],
+   [0,0,0] 
+what we need: 
+    Take all the paths, 
+    keep track of how many steps it takes us.
+    track of visited 
+
+approch --> dp approch -> breadth first search using queue will be enough i think
+
+'''
 
 class Solution:
     def shortestPath(self, grid: List[List[int]], k: int) -> int:
-        rows, cols = len(grid), len(grid[0])
-        directions = [(1, 0), (-1, 0), (0, 1), (0, -1)]
-        
-        # If k is enough to go straight without worrying about obstacles
-        if k >= rows + cols - 2:
-            return rows + cols - 2
-        
-        queue = deque([(0, 0, k, 0)])  # (row, col, remaining_k, steps)
-        visited = {(0, 0, k)}
-        
-        while queue:
-            i, j, rem, steps = queue.popleft()
+        ROWS, COLS = len(grid), len(grid[0])
+
+        # (i, j, k, steps)
+        nodes = deque([(0, 0, k, 0)]) # start node
+        visited = {(0, 0, k, 0)}
+
+        directions = [(0, 1), (1, 0), (-1, 0), (0, -1)]
+
+        while nodes: 
+            x, y, rem_k, steps = nodes.popleft()
             
-            # reached destination
-            if (i, j) == (rows - 1, cols - 1):
+            if (x, y) == (ROWS-1, COLS-1):
                 return steps
-            
+
             for dx, dy in directions:
-                x, y = i + dx, j + dy
-                if 0 <= x < rows and 0 <= y < cols:
-                    new_k = rem - grid[x][y]
-                    if new_k >= 0 and (x, y, new_k) not in visited:
-                        visited.add((x, y, new_k))
-                        queue.append((x, y, new_k, steps + 1))
-        
-        return -1  # no path
+                nx, ny = dx + x, dy + y
+                # if in bound
+                if 0 <= nx < ROWS and 0 <= ny < COLS:
+                    new_k = rem_k - grid[nx][ny]
+                
+                    if new_k >= 0 and (nx, ny, new_k) not in visited:
+                        visited.add((nx, ny, new_k))
+                        nodes.append((nx, ny, new_k, steps + 1))
+                    
+
+        return -1
+
