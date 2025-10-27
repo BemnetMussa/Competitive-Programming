d02bf41916bad477fe9b08cdeec29d15e62ccb0a
Bemnet Mussa <117770007+BemnetMussa@users.noreply.github.com>
2025-10-27 16:16:11 +0300

    Time: 0 ms (100.00%) | Memory: 17.6 MB (98.71%) - LeetSync

--- 13-roman-to-integer/roman-to-integer.py
+++ 13-roman-to-integer/roman-to-integer.py
@@ -1,32 +1,59 @@
+'''
+Given: change from roman integer. 
+Symbol       Value
+I             1
+V             5
+X             10
+L             50
+C             100
+D             500
+M             1000
+
+"LVIII"
+ |     -> 50 + 5 + 3 = 58
+
+"MCMXCIV"
+ |     -> 1000 
+  |    -> 1000 + 100
+   |   _> 1000 + (100 < 1000 = substract prev with curr) 900
+    |  -> 1000 + 900 + 10
+     | -> 1000 + 900 + (X < c) = 90
+      | -> 1000 + 900 + 90 + 1
+       | -> 1000 + 900 + 90 + (I < V) = 4
+
+1000 + 900 + 90 + 4 = 1994
+
+needs:
+ - dictionary holds the key: value -> roman symbols
+ - iterating through it and checking 
+ - stack
+
+'''
+
+
 class Solution:
     def romanToInt(self, s: str) -> int:
-        roman_dict = {
-            "I":             1,
-            "V":             5,
-            "X":             10,
-            "L":             50,
-            "C":             100,
-            "D":             500,
-            "M":             1000
+        dictionary = {
+            "I": 1,
+            "V": 5,
+            "X": 10,
+            "L": 50,
+            "C": 100,
+            "D": 500,
+            "M": 1000
         }
 
-        prev_num = 0
-        ans = 0
+        stack = [] # answer 
+
+        for char in s:
+            val = dictionary[char]
+            if stack and stack[-1] < val:
+                prev_val = stack.pop()
+                val = val - prev_val
+
+            stack.append(val)
+
+        return sum(stack) # Time: O(N) and Space: O(N)
 
 
-        for i in range(len(s)-1, -1, -1):
-            
-            value = roman_dict[s[i]]
-
-            if value >= prev_num:
-                ans += value
-            else:
-
-                ans -= value
-
-            prev_num = value 
-
-
-        return ans
-
-
+        
