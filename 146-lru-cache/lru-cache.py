dd4bcf2fd1e2e5409238b981bfd2fe112adcf019
Bemnet Mussa <117770007+BemnetMussa@users.noreply.github.com>
2025-10-30 16:20:32 +0300

    Time: 140 ms (53.65%) | Memory: 78.8 MB (8.87%) - LeetSync

--- 146-lru-cache/lru-cache.py
+++ 146-lru-cache/lru-cache.py
@@ -1,67 +1,83 @@
+'''
+Design a data structrue the follows the LRU chache.
+properties LRU cahce should have
+- LRUCahce(int capacity) -> define a fixed capacity
+- int get(int key) -> return the value of the key if it exists otherwise *-1
+- int put(key, value) -> update a value of the key if th key exsits otherwise add the key. 
+            - ** if the capacity have arleady reached remove the least recently used one and replacemethods.
+
+* must be implemented in O(1) time all of the methods.
+
+approch: 
+using linked list + map
+all the operations in O(1)
+'''
+
 class Node:
-    def __init__(self,key=0, value=0,next=None, prev=None):
-        self.val = value
-        self.next = next
-        self.prev = prev
+    def __init__(self, key=0, value=0):
         self.key = key
+        self.val = value
+        self.next = None
+        self.prev = None
 
-class LinkedList:
-    def __init__(self):
-        self.head = Node()
-        self.tail = Node()
-
-        self.head.next = self.tail
-        self.tail.prev = self.head
-
-    def add(self,node):
-        previous_node = self.tail.prev
-        previous_node.next = node
-        node.prev = previous_node
-        node.next = self.tail
-        self.tail.prev = node
-    
-    def remove(self,node):
-        previous_node = node.prev
-        previous_node.next = node.next
-        node.next.prev = previous_node
-    
-    def getHead(self):
-        return self.head.next
-
+class Node:
+    def __init__(self, key=0, value=0):
+        self.key = key
+        self.val = value
+        self.prev = None
+        self.next = None
 
 class LRUCache:
-
     def __init__(self, capacity: int):
-        self.order = LinkedList()
-        self.cache = {}
+        self.head = Node()
+        self.tail = Node()
+        self.head.next = self.tail
+        self.tail.prev = self.head
         self.capacity = capacity
-        
+        self.map = {}
+
+    def _remove(self, node):
+        prev, nxt = node.prev, node.next
+        prev.next = nxt
+        nxt.prev = prev
+
+    def _addToHead(self, node):
+        node.next = self.head.next
+        self.head.next.prev = node
+        self.head.next = node
+        node.prev = self.head
 
     def get(self, key: int) -> int:
-        if key not in self.cache:
-            return -1
-        
-        node = self.cache[key]
-        self.order.remove(node)
-        self.order.add(node)
-        return node.val
-        
+        if key in self.map:
+            node = self.map[key]
+            self._remove(node)
+            self._addToHead(node)
+            return node.val
+        return -1
 
     def put(self, key: int, value: int) -> None:
-        if key in self.cache:
-            node = self.cache[key]
+        if key in self.map:
+            node = self.map[key]
             node.val = value
-            self.order.remove(node) 
-            self.order.add(node)
+            self._remove(node)
+            self._addToHead(node)
         else:
-            if self.capacity == len(self.cache):
-                head = self.order.getHead()
-                self.order.remove(head)
-                self.cache.pop(head.key)
-            node = Node(key,value)
-            self.order.add(node)
-            self.cache[key] = node
+            if len(self.map) == self.capacity:
+                lru_node = self.tail.prev
+                self._remove(lru_node)
+                del self.map[lru_node.key]
+
+            new_node = Node(key, value)
+            self.map[key] = new_node
+            self._addToHead(new_node)
+
+
+
+            
+    
         
+
+
         
 
 
