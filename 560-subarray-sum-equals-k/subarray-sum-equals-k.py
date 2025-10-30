a9cae7495633a1a3a96e36119c23005b518b1211
Bemnet Mussa <117770007+BemnetMussa@users.noreply.github.com>
2025-10-30 12:29:27 +0300

    Time: 33 ms (49.92%) | Memory: 20.4 MB (71.67%) - LeetSync

--- 560-subarray-sum-equals-k/subarray-sum-equals-k.py
+++ 560-subarray-sum-equals-k/subarray-sum-equals-k.py
@@ -1,16 +1,30 @@
 class Solution:
     def subarraySum(self, nums: List[int], k: int) -> int:
+        # [1, 2, 3] 
+        # subarryes
+        #   1, 1, 1
+        #   1, 2, 1
+        #   1 , 2, 2
+        #   base case total == k
+        # count --> dp approch (recursive approch)
+        # time compelxity on each recursive call we have n options so recrusively it iwll be n^n which will fail
+        
+        # [1, 1, 1]
+        #        | 
+        #  0, 0, 0, 0 base case 1
+        # why not just bottom up where we track the needed difference to make it valid so
+
+        prefix = {}
+        count = 0
+        prefix[0] = 1 # base case
 
         curr_sum = 0
-        prefix_sum = {0: 1}
-        res = 0
-
-        for nums in nums:
-            curr_sum += nums
+        for num in nums:
+            curr_sum += num
             diff = curr_sum - k
 
-            res += prefix_sum.get(diff, 0)
+            count += prefix.get(diff, 0)
 
-            prefix_sum[curr_sum] =  1 + prefix_sum.get(curr_sum, 0)
+            prefix[curr_sum] = prefix.get(curr_sum, 0) + 1
 
-        return res
+        return count
