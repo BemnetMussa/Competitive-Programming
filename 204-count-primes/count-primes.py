b273cc52f278045d183d332007ff96e10d5a723c
Bemnet Mussa <117770007+BemnetMussa@users.noreply.github.com>
2025-11-11 18:16:49 +0300

    Time: 1218 ms (78.50%) | Memory: 56.3 MB (84.19%) - LeetSync

--- 204-count-primes/count-primes.py
+++ 204-count-primes/count-primes.py
@@ -1,24 +1,17 @@
 class Solution:
     def countPrimes(self, n: int) -> int:
-        
-        # given n: Int, 
-        # requested to get the number prime numbers less than n
-
-        # approch:
-            # factorize the primes from n upto 2 and return the number of prime numbers
-            # or iterate from 2 upto sqrt(n) and track the number of prime numbers since it will be valid anytime but more than sqrt(n) also it is less than n so we should go upt to n
-            # it will take Time complexity of O(n) and space of O(n)
 
 
-        if n < 2:
+
+        if n <= 2:
             return 0
-
-        is_prime = [True] * n
+        
+        is_prime = [True] * n # all equal
         is_prime[0] = is_prime[1] = False
-
-        for i in range(2, int(n ** 0.5) + 1):
+        for i in range(2, int(n ** 0.5) +1):
             if is_prime[i]:
-                for j in range(i * i, n, i):
+                for j in range(i*i, n, i):
                     is_prime[j] = False
+
 
         return sum(is_prime)
