287fd4405c4cf8491d0b9ebb4a5b82bca991771b
Bemnet Mussa <117770007+BemnetMussa@users.noreply.github.com>
2025-11-25 22:33:06 +0300

    Time: 965 ms (22.28%) | Memory: 18 MB (38.86%) - LeetSync

--- 1064-smallest-integer-divisible-by-k/smallest-integer-divisible-by-k.py
+++ 1064-smallest-integer-divisible-by-k/smallest-integer-divisible-by-k.py
@@ -4,22 +4,21 @@ class Solution:
         if not (k%2 and k%5):
             return -1
 
-        rem = 1
-        count = 1
-    
-        while rem%k:
-            N = rem *10 + 1
-            rem = N%k
-            count += 1
+        # rem = 1
+        # count = 1
+        n = 1
+        for i in range(1, k+1):
+            if n % k == 0:
+                return i
+            n = n*10 + 1
+        return i
+        
+
+        # while rem%k:
+        #     N = rem *10 + 1
+        #     rem = N%k
+        #     count += 1
+        #     print(N, rem,count)
             
         
-        return count
-
-
-    # n = 1
-    #     for i in range(1, k+1):
-    #         if n % k == 0:
-    #             return i
-    #         n = n*10 + 1
-    #     return i
-        
+        # # return count
