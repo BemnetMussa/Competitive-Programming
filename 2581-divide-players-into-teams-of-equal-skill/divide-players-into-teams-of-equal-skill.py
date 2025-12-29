62c46af3984f37dc19b95e1019a191515829b08e
Bemnet Mussa <117770007+BemnetMussa@users.noreply.github.com>
2025-12-29 17:47:31 +0300

    Time: 83 ms (8.51%) | Memory: 28.5 MB (76.60%) - LeetSync

--- 2581-divide-players-into-teams-of-equal-skill/divide-players-into-teams-of-equal-skill.py
+++ 2581-divide-players-into-teams-of-equal-skill/divide-players-into-teams-of-equal-skill.py
@@ -1,26 +1,33 @@
 class Solution:
     def dividePlayers(self, skill: List[int]) -> int:
+        N = len(skill)
+        if N % 2 != 0:
+            return -1
 
-        skill.sort()
+        # nive approch will take O(nlogn)
+        # time taken --> 10^5
+        # [3, 2, 5, 1, 3, 4] -> total = 18 // 3 = 6
+        # hash = [ ]
+        # 3 - 6 = abs(-3) = 3 x
+        # 2- 6 = abs(-4]) = 4 X
+        # 5 - 6 = abs(-1) = 1 X
+        # 1 - 6 = abs(-5) = 5 corret -> 1
+        # 3 - 6 = abs(-3) = 3 correct -> 2
+        # 4 - 6 = abs(-2) = 2 correct -> 3
+        # since -> 3 == len(skill) / 2 -> valid return true
+        # Time -> O(n), space O(n/2) = O(n)
 
-        left = 1
-        right = len(skill)-2
+        total = sum(skill)
+        target = total // (len(skill) // 2 )
+        hashmap = Counter()
+        teamUp = []
 
-        chem = skill[0] + skill[-1]
-
-
-        prod = skill[0] * skill[-1]
-        while left < right:
-
-            if skill[left] + skill[right] == chem:
-                prod += (skill[left] * skill[right])
+        for num in skill:
+            difference = target - num
+            if hashmap[difference] > 0:
+                hashmap[difference] -= 1
+                teamUp.append(difference * num)
             else:
-                return -1
-            left += 1
-            right -= 1
+                hashmap[num] += 1
 
-
-        return prod
-
-
-            
+        return sum(teamUp) if len(teamUp) == len(skill) // 2 else -1
