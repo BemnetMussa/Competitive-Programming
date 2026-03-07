fd8a2a0da51de786ca87bb41aca08d02e0d8660c
Bemnet Mussa <117770007+BemnetMussa@users.noreply.github.com>
2026-03-07 10:56:02 +0300

    Time: 0 ms (100.00%) | Memory: 19.3 MB (72.49%) - LeetSync

--- 62-unique-paths/unique-paths.py
+++ 62-unique-paths/unique-paths.py
@@ -8,21 +8,36 @@ class Solution:
         3. baseCase:
                     when we reach the (m-1, n-1). 
         '''
-        memo = {}
-        def dp(i, j):
-            # basecase
-            if not(0 <= i < m) or not(0 <= j < n):
-                return 0
-            if (i, j) == (m-1, n-1): # final postioin 
-                return 1
+        # memo = {}
+        # def dp(i, j):
+        #     # basecase
+        #     if not(0 <= i < m) or not(0 <= j < n):
+        #         return 0
+        #     if (i, j) == (m-1, n-1): # final postioin 
+        #         return 1
 
-            if (i, j) in memo:
-                return memo[(i, j)]
-            # Transitoin
-            answer = dp(i+1, j) + dp(i, j+1)
+        #     if (i, j) in memo:
+        #         return memo[(i, j)]
+        #     # Transitoin
+        #     answer = dp(i+1, j) + dp(i, j+1)
+        #     memo[(i, j)] = answer
+        #     return answer
+        # return dp(0, 0)
 
-            memo[(i, j)] = answer
-            # result
-            return answer
+        # 1. state space
+        dp = [[0 for _ in range(n)] for _ in range(m)]
+        
+        # 2. intializing the base case
+        for i in range(m):
+            for j in range(n):
+                dp[i][j] = 1
 
-        return dp(0, 0)
+        # 3. transition
+        for i in range(1, m):
+            for j in range(1, n):
+                dp[i][j] = dp[i-1][j] + dp[i][j-1]
+
+        # 4. result
+        return dp[m-1][n-1]
+
+        
