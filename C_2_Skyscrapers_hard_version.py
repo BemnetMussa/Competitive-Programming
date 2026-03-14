36758a13bd97a810d6ebea4cbc11b055086b882a
Bemnetmussa <bemnetmussa@gmail.com>
2026-03-14 23:49:43 +0300

    Refactor C2 Skyscrapers solution for improved readability and consistency

--- C_2_Skyscrapers_hard_version.py
+++ C_2_Skyscrapers_hard_version.py
@@ -1,50 +1,56 @@
-import sys
-input = sys.stdin.readline
 
 n = int(input())
 m = list(map(int, input().split()))
 
-L = [0]*n
-R = [0]*n
+L = [0] * n
+R = [0] * n
+
 stack = []
 
-# left sums
+# From the left to the right -> increase  - peak - decrease
 for i in range(n):
     while stack and m[stack[-1]] > m[i]:
         stack.pop()
-
     if stack:
         j = stack[-1]
-        L[i] = L[j] + m[i]*(i-j)
+        L[i] = L[j] + (i-j) * m[i] # increasing order fro all i -> j indexis 
     else:
-        L[i] = m[i]*(i+1)
+        L[i] = (i+1) * m[i]
 
     stack.append(i)
 
-stack.clear()
+stack = []
 
-# right sums
-for i in range(n-1,-1,-1):
-    while stack and m[stack[-1]] > m[i]:
+# From the left to the right -> Decrease - peak - increase
+for i in range(n-1, -1, -1):
+    while stack and m[stack[-1]]     > m[i]:
         stack.pop()
-
     if stack:
         j = stack[-1]
-        R[i] = R[j] + m[i]*(j-i)
+        R[i] = R[j] + (j-i) * m[i]
     else:
-        R[i] = m[i]*(n-i)
+        R[i] = (n-i)*m[i]
 
     stack.append(i)
 
-peak = max(range(n), key=lambda i: L[i]+R[i]-m[i])
+# find best peak
+best = 0 
+peak = 0
 
-a = [0]*n
+for i in range(n):
+    val = L[i]+ R[i] - m[i]
+    if val > best:
+        best = val
+        peak = i
+
+# reconstrcut answer
+a = [0] * n
 a[peak] = m[peak]
 
-for i in range(peak-1,-1,-1):
+for i in range(peak-1, -1, -1):
     a[i] = min(m[i], a[i+1])
 
-for i in range(peak+1,n):
+for i in range(peak+1, n):
     a[i] = min(m[i], a[i-1])
 
 print(*a)
