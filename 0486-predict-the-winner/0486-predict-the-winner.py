1becadff257b0958b1b3f603be7ffe8352a26fbf
Bemnet Mussa <117770007+BemnetMussa@users.noreply.github.com>
2026-03-19 19:26:00 +0300

    Time: 1591 ms (16.79%), Space: 19.2 MB (92.12%) - LeetHub

---
+++ 0486-predict-the-winner/0486-predict-the-winner.py
@@ -0,0 +1,14 @@
+class Solution:
+    def predictTheWinner(self, nums: List[int]) -> bool:
+        n = len(nums)
+
+        def gamE(left: int, right: int) -> int:
+            if left == right:
+                return nums[left]
+            
+            score_left = nums[left] - gamE(left + 1, right)
+            score_right = nums[right] - gamE(left, right - 1)
+            return max(score_left, score_right)
+        
+
+        return gamE(0, n - 1) >= 0
