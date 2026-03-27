0623a2518e12c57c622ccfbda08eff3f69164e26
Bemnet Mussa <117770007+BemnetMussa@users.noreply.github.com>
2026-03-27 17:33:14 +0300

    Time: 7 ms (96.07%) | Memory: 19.6 MB (81.51%) - LeetSync

--- 51-n-queens/n-queens.py
+++ 51-n-queens/n-queens.py
@@ -1,35 +1,31 @@
 class Solution:
-    def solveNQueens(self, n: int) -> List[List[str]]:
-        cols = set()
-        postivDiag = set()
-        negativeDiag = set()
+    def solveNQueens(self, n: int):
+        cols = [False] * n
+        posDiag = [False] * (2 * n - 1)   # r + c
+        negDiag = [False] * (2 * n - 1)   # r - c + (n-1)
 
         board = [["."] * n for _ in range(n)]
+        res = []
 
-        res= []
         def backtrack(r):
-            #base case
             if r == n:
-                copy = ["".join(row) for row in board]
-                res.append(copy)
+                res.append(["".join(row) for row in board])
                 return
-            
-            for c in range(n): # for each row
-                if c in cols or (r + c) in postivDiag or (r - c) in negativeDiag:
+
+            for c in range(n):
+                pd = r + c
+                nd = r - c + (n - 1)
+
+                if cols[c] or posDiag[pd] or negDiag[nd]:
                     continue
-                
-                cols.add(c)
-                postivDiag.add(r+c)
-                negativeDiag.add(r-c)
+
+                cols[c] = posDiag[pd] = negDiag[nd] = True
                 board[r][c] = "Q"
 
-                backtrack( r + 1 ) # going to the next row
+                backtrack(r + 1)
 
-                cols.remove(c)
-                postivDiag.remove(r + c)
-                negativeDiag.remove(r-c)
                 board[r][c] = "."
+                cols[c] = posDiag[pd] = negDiag[nd] = False
 
         backtrack(0)
         return res
-    
